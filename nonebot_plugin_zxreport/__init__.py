e8c2be7cefdceea5193ba0f9ec1af93cd5ce016d
HibiKier <775757368@qq.com>
2024-10-16 09:36:45 +0800

    🎨 代码优化

--- nonebot_plugin_zxreport/__init__.py
+++ nonebot_plugin_zxreport/__init__.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
 from nonebot import require
 from nonebot.log import logger
+from nonebot.permission import SUPERUSER
 from nonebot.plugin import PluginMetadata, inherit_supported_adapters
+from playwright.async_api import TimeoutError
 
 require("nonebot_plugin_alconna")
 require("nonebot_plugin_htmlrender")
@@ -8,6 +12,7 @@ require("nonebot_plugin_localstore")
 
 from nonebot_plugin_alconna import Alconna, Image, UniMessage, on_alconna
 
+from .config import REPORT_PATH
 from .data_source import Report
 
 __plugin_meta__ = PluginMetadata(
@@ -28,9 +33,26 @@ __plugin_meta__ = PluginMetadata(
 
 _matcher = on_alconna(Alconna("真寻日报"), priority=5, block=True)
 
+_reset_matcher = on_alconna(
+    Alconna("重置真寻日报"), priority=5, block=True, permission=SUPERUSER
+)
+
+
+@_reset_matcher.handle()
+async def _():
+    file = REPORT_PATH / f"{datetime.now().date()}.png"
+    if file.exists():
+        file.unlink()
+        logger.info("重置真寻日报")
+    await UniMessage("真寻日报已重置!").send()
+
 
 @_matcher.handle()
 async def _():
-    path = await Report.get_report_image()
-    await UniMessage(Image(path=path)).send()
-    logger.info("查看真寻日报")
+    try:
+        path = await Report.get_report_image()
+        await UniMessage(Image(path=path)).send()
+        logger.info("查看真寻日报")
+    except TimeoutError:
+        await UniMessage("真寻日报生成超时...").send(at_sender=True)
+        logger.error("真寻日报生成超时")
